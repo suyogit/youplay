5a72a9f347ad0452cb8b2d50f3b973b826de166a
Suyog Acharya <asuyog042@gmail.com>
2023-10-09 18:13:09 +0545

    adding input in chat created

--- src/component/LiveChat.js
+++ src/component/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
@@ -7,6 +7,7 @@ import { generateRandomName, generateRandomString } from "../utils/helper";
 
 
 const LiveChat = () => {
+  const [liveMessages, setLiveMessages] = useState([]); 
   const chatMessages = useSelector((store) => store.chat.messages);
 
   const dispatch = useDispatch();
@@ -36,7 +37,7 @@ const LiveChat = () => {
 
 
   return (
-    <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+    <>    <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
 
       {
         chatMessages.map((c, i) => (
@@ -44,6 +45,39 @@ const LiveChat = () => {
         ))
       }
     </div>
+
+      <form className="w-full p-2 ml-2 border border-black"
+
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Suyog Acharya",
+              message: liveMessages,
+            })
+          )
+          setLiveMessages("")
+        }
+        }
+
+      >
+        <input
+          className=" px-2 w-80"
+          type="text"
+
+          value={liveMessages}
+          onChange={(e) => setLiveMessages(e.target.value)}
+          placeholder="Enter your message..."
+        />
+        <button className="bg-green-300 hover:bg-green-700 text-white font-bold py-2 px-2  mx-2 rounded">
+          Send
+        </button>
+
+      </form>
+
+
+    </>
+
   );
 };
 
